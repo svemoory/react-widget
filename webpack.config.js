dbde3a61203431190c765e9926ce1a0746ef8678
Surekha Vemoory <svemoory@mlslistings.com>
2017-05-24 16:39:20 -0700

    reactstrap external dependancies

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,31 @@ module.exports = {
  { test: /\.(png|jpg|jpeg|gif|woff)$/, loader: 'url-loader?limit=8192' }
     ]
   },
-
+    externals: [
+      {
+        react: {
+          root: 'React',
+          commonjs2: 'react',
+          commonjs: 'react',
+          amd: 'react'
+        }
+      },
+      {
+        'react-dom': {
+          root: 'ReactDOM',
+          commonjs2: 'react-dom',
+          commonjs: 'react-dom',
+          amd: 'react-dom'
+        }
+      },
+      {
+        'react-transition-group': {
+          commonjs: 'react-transition-group',
+          commonjs2: 'react-transition-group',
+          amd: 'react-transition-group',
+        }
+      }
+    ],
 
   plugins: [
     //new ExtractTextPlugin({ filename: 'css/[name].css', disable: false, allChunks: true }),
